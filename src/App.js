9f5af06930b76cabb7cbc7dc715ed71f9d879ef1
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-05-18 13:06:29 -0500

    Add functionality to App component for displaying random animals. Implemented state management and button to add animals to the display.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+//generates a random animal and returns the animal array
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 const App = () => {
-  return <div>Show Animals Here</div>;
+  //declare animal state
+  const [animals, setAnimals] = useState([]);
+
+  //change animal state when button is clicked
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  //render the results
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
+    </div>
+  );
 };
 
 export default App;
