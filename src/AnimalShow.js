caf3b665d529fdb7b6a9172684f6381f4f1c14e8
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-05-18 13:18:08 -0500

    Add SVG assets for various animals and update AnimalShow component to render corresponding SVG images based on the animal type prop.

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,28 @@
 import React from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  heart,
+  horse,
+};
 
 const AnimalShow = ({ type }) => {
-  return <div>{type}</div>;
+  return (
+    <div>
+      <img src={svgMap[type]} />
+    </div>
+  );
 };
 
 export default AnimalShow;
